876f8da32caa55b2778b4c26c535fe410e124831
saphirevert <jerome.cosandey@gmail.com>
2022-04-07 11:08:55 +0200

    [fix]  Add constructor to User.cs and UserDto.cs

--- app/Models/DTO/UserDto.cs
+++ app/Models/DTO/UserDto.cs
@@ -1,11 +1,22 @@
 public class UserDto
 {
     public int Id { get; set; }
-    public int Mail { get; set; }
+    public string Mail { get; set; }
     public string Name { get; set; }
 
     public string Surname { get; set; }
-    public int Address { get; set; }
+    public string Address { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
+
+    public UserDto(){}
+    public UserDto(User user){
+        this.Id = user.Id;
+        this.Mail = user.Mail;
+        this.Name = user.Name;
+        this.Surname = user.Surname;
+        this.Address = user.Address;
+        this.City = user.City;
+        this.Country = user.Country;
+    }
 }
--- app/Models/User.cs
+++ app/Models/User.cs
@@ -1,11 +1,20 @@
 public class User
 {
     public int Id { get; set; }
-    public int Mail { get; set; }
+    public string Mail { get; set; }
     public string Name { get; set; }
-
     public string Surname { get; set; }
-    public int Address { get; set; }
+    public string Address { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
+
+    public User(){}
+    public User(UserDto userDto){
+        Mail = userDto.Mail;
+        Name = userDto.Name;
+        Surname = userDto.Surname;
+        Address = userDto.Address;
+        City = userDto.City;
+        Country = userDto.Country;
+    }
 }
